7ee25d174ac34a85789517a9a372351260a62a28
bratus <alajoan.jonathan@gmail.com>
2015-07-26 21:15:12 -0300

    123123123123
    12312312312312

---
+++ Assets/Scripts/ComecaJogo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComecaJogo : MonoBehaviour {
+
+	bool comecaJogo = false;
+	public GameObject botaoPlay;
+	void Start () {
+	
+	}
+	
+	public void comecaJoguinho()
+	{
+		comecaJogo = true;
+		if (comecaJogo == true) {
+			Application.LoadLevel ("asda");
+		}
+		
+	}
+}
